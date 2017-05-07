c0a7a81f6457b8add5740263bd0ff8917dc7129a
Equationist <ummonk@gmail.com>
2017-05-06 20:17:48 -0700

    fix pq allocation

--- pqueue_test.c
+++ pqueue_test.c
@@ -19,106 +19,106 @@ int pqueue_test()
 
     int l;
 
-    int_pqueue* pq;
-    int_pqueue_init(pq);
-    assert(pq->length == 0);
+    int_pqueue pq;
+    int_pqueue_init(&pq);
+    assert(&pq.length == 0);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 10);
-    assert(int_pqueue_peek(pq) == 10);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 10);
+    assert(int_pqueue_peek(&pq) == 10);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 5);
-    assert(int_pqueue_peek(pq) == 10);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 5);
+    assert(int_pqueue_peek(&pq) == 10);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 25);
-    assert(int_pqueue_peek(pq) == 25);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 25);
+    assert(int_pqueue_peek(&pq) == 25);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 7);
-    assert(int_pqueue_peek(pq) == 25);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 7);
+    assert(int_pqueue_peek(&pq) == 25);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 13);
-    assert(int_pqueue_peek(pq) == 25);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 13);
+    assert(int_pqueue_peek(&pq) == 25);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 7);
-    assert(int_pqueue_peek(pq) == 25);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 7);
+    assert(int_pqueue_peek(&pq) == 25);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 17);
-    assert(int_pqueue_peek(pq) == 25);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 17);
+    assert(int_pqueue_peek(&pq) == 25);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 50);
-    assert(int_pqueue_peek(pq) == 50);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 50);
+    assert(int_pqueue_peek(&pq) == 50);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 60);
-    assert(int_pqueue_peek(pq) == 60);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 60);
+    assert(int_pqueue_peek(&pq) == 60);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 1);
-    assert(int_pqueue_peek(pq) == 60);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 1);
+    assert(int_pqueue_peek(&pq) == 60);
+    assert(&pq.length = l+1);
 
     printf("Testing pqueue pops!\n");
 
-    l = pq->length;
-    assert(int_pqueue_pop(pq) == 60);
-    assert(pq->length = l-1);
-    assert(int_pqueue_peek(pq) == 50);
+    l = pq.length;
+    assert(int_pqueue_pop(&pq) == 60);
+    assert(&pq.length = l-1);
+    assert(int_pqueue_peek(&pq) == 50);
 
-    l = pq->length;
-    assert(int_pqueue_pop(pq) == 50);
-    assert(pq->length = l-1);
-    assert(int_pqueue_peek(pq) == 25);
+    l = pq.length;
+    assert(int_pqueue_pop(&pq) == 50);
+    assert(&pq.length = l-1);
+    assert(int_pqueue_peek(&pq) == 25);
 
-    l = pq->length;
-    assert(int_pqueue_pop(pq) == 25);
-    assert(pq->length = l-1);
-    assert(int_pqueue_peek(pq) == 17);
+    l = pq.length;
+    assert(int_pqueue_pop(&pq) == 25);
+    assert(&pq.length = l-1);
+    assert(int_pqueue_peek(&pq) == 17);
 
-    l = pq->length;
-    assert(int_pqueue_pop(pq) == 17);
-    assert(pq->length = l-1);
-    assert(int_pqueue_peek(pq) == 13);
+    l = pq.length;
+    assert(int_pqueue_pop(&pq) == 17);
+    assert(&pq.length = l-1);
+    assert(int_pqueue_peek(&pq) == 13);
 
-    l = pq->length;
-    assert(int_pqueue_pop(pq) == 13);
-    assert(pq->length = l-1);
-    assert(int_pqueue_peek(pq) == 10);
+    l = pq.length;
+    assert(int_pqueue_pop(&pq) == 13);
+    assert(&pq.length = l-1);
+    assert(int_pqueue_peek(&pq) == 10);
 
-    l = pq->length;
-    assert(int_pqueue_pop(pq) == 10);
-    assert(pq->length = l-1);
-    assert(int_pqueue_peek(pq) == 7);
+    l = pq.length;
+    assert(int_pqueue_pop(&pq) == 10);
+    assert(&pq.length = l-1);
+    assert(int_pqueue_peek(&pq) == 7);
 
-    l = pq->length;
-    assert(int_pqueue_pop(pq) == 7);
-    assert(pq->length = l-1);
-    assert(int_pqueue_peek(pq) == 5);
+    l = pq.length;
+    assert(int_pqueue_pop(&pq) == 7);
+    assert(&pq.length = l-1);
+    assert(int_pqueue_peek(&pq) == 5);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 3);
-    assert(int_pqueue_peek(pq) == 5);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 3);
+    assert(int_pqueue_peek(&pq) == 5);
+    assert(&pq.length = l+1);
 
-    l = pq->length;
-    int_pqueue_insert(pq, 20);
-    assert(int_pqueue_peek(pq) == 20);
-    assert(pq->length = l+1);
+    l = pq.length;
+    int_pqueue_insert(&pq, 20);
+    assert(int_pqueue_peek(&pq) == 20);
+    assert(&pq.length = l+1);
 
     return 1;
 }
